bf18af79098f30cf3ccdc8aeed9edabf0ebe44a1
ilham agung <plato.ilham@gmail.com>
2018-09-08 11:20:04 +0700

    last result (udah jalan)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
+import VueFire from 'vuefire'
 
+Vue.use(VueFire)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  template: '<App/>',
   render: h => h(App)
 })
